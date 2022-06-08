2aca22d7e247c21a99570d79297191bbbf4723f2
zyzera <herbachukzachary@gmail.com>
2022-06-08 16:25:16 -0500

    Initial commit

---
+++ Assets/TerrainGenerator.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    public int[,] mapArray;
+    [SerializeField] Tile baseTile;
+    Tilemap tilemap;
+    public int terrainWidth, terrainHeight;
+
+
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateTerrain();
+        }
+    }
+
+
+    public static int[,] GenerateArray(int width, int height, bool empty)
+    {
+        int[,] map = new int[width, height];
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                if (empty)
+                {
+                    map[x, y] = 0;
+                }
+                else
+                {
+                    map[x, y] = 1;
+                }
+            }
+        }
+        return map;
+    }
+
+    public static void RenderMap(int[,] map, Tilemap tilemap, TileBase tile)
+    {
+        //Clear the map (ensures we dont overlap)
+        tilemap.ClearAllTiles();
+        //Loop through the width of the map
+        for (int x = 0; x < map.GetUpperBound(0) ; x++)
+        {
+            //Loop through the height of the map
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                // 1 = tile, 0 = no tile
+                if (map[x, y] == 1)
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+            }
+        }
+    }
+
+    public static void UpdateMap(int[,] map, Tilemap tilemap) //Takes in our map and tilemap, setting null tiles where needed
+    {
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                //We are only going to update the map, rather than rendering again
+                //This is because it uses less resources to update tiles to null
+                //As opposed to re-drawing every single tile (and collision data)
+                if (map[x, y] == 0)
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), null);
+                }
+            }
+        }
+    }
+
+    public static int[,] PerlinNoise(int[,] map, float seed)
+    {
+        int newPoint;
+        //Used to reduced the position of the Perlin point
+        float reduction = 0.5f;
+        //Create the Perlin
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            newPoint = Mathf.FloorToInt((Mathf.PerlinNoise(x, seed) - reduction) * map.GetUpperBound(1));
+
+            //Make sure the noise starts near the halfway point of the height
+            newPoint += (map.GetUpperBound(1) / 2);
+            for (int y = newPoint; y >= 0; y--)
+            {
+                map[x, y] = 1;
+            }
+        }
+        return map;
+    }
+
+    void GenerateTerrain()
+    {
+        float seed = Random.Range(.1f, 1f);
+        mapArray = GenerateArray(terrainWidth, terrainHeight, true);
+        RenderMap(PerlinNoise(mapArray, seed), map, tile);
+    }
+}
